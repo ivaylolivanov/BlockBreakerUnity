c4726e1627f4ebef53aee2c4597c3f837c41f23d
ivaylolivanov <ivaylolivanov95@gmail.com>
2020-10-19 13:14:37 +0300

    Win Screen, SceneLoader feature
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneLoader : MonoBehaviour
 {
+
+    [SerializeField] public string winScreenName = "WinScreen";
+    [SerializeField] public float winScreenScorePosX;
+    [SerializeField] public float winScreenScorePosY;
+
     public void LoadNextScene() {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(++currentSceneIndex);
         Cursor.visible = false;
+
+        string nextSceneName = SceneManager.GetSceneByBuildIndex(
+            currentSceneIndex
+        ).name;
+        if(nextSceneName == winScreenName) {
+            ReorderWinScreen();
+        }
     }
 
     public void LoadStartMenuScene() {
@@ -17,5 +30,17 @@ public class SceneLoader : MonoBehaviour
 
     public void Quit() {
         Application.Quit();
+    }
+
+    private void ReorderWinScreen() {
+        TextMeshProUGUI scoreText = FindObjectOfType<TextMeshProUGUI>();
+
+        scoreText.transform.position = new Vector3(
+            winScreenScorePosX,
+            winScreenScorePosY,
+            0
+        );
+
+        Cursor.visible = true;
     }
 }
--- Temp/StagingArea/Data/il2cppOutput/lumpedcpp/Lump_libil2cpp_gc.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "/home/m00ns7ruck/Unity/Hub/Editor/2019.4.9f1/Editor/Data/il2cpp/libil2cpp/gc/BoehmGC.cpp"
-#include "/home/m00ns7ruck/Unity/Hub/Editor/2019.4.9f1/Editor/Data/il2cpp/libil2cpp/gc/GCHandle.cpp"
-#include "/home/m00ns7ruck/Unity/Hub/Editor/2019.4.9f1/Editor/Data/il2cpp/libil2cpp/gc/GarbageCollector.cpp"
-#include "/home/m00ns7ruck/Unity/Hub/Editor/2019.4.9f1/Editor/Data/il2cpp/libil2cpp/gc/NullGC.cpp"
-#include "/home/m00ns7ruck/Unity/Hub/Editor/2019.4.9f1/Editor/Data/il2cpp/libil2cpp/gc/WriteBarrier.cpp"
-#include "/home/m00ns7ruck/Unity/Hub/Editor/2019.4.9f1/Editor/Data/il2cpp/libil2cpp/gc/WriteBarrierValidation.cpp"
